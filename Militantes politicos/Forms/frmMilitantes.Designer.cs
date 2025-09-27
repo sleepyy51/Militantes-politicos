b1a9103d9a4ace7f23100398bbf974c726eeba26
sleepyy <sleepyy@DESKTOP-FGANC6G>
2025-09-27 14:16:10 -0600

    Agregar archivos de proyecto.

---
+++ Militantes politicos/Forms/frmMilitantes.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace Militantes_politicos
+{
+    partial class frmMilitantes
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.msMenu = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cargarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.opcionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.limpiarDatosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.labEstado = new System.Windows.Forms.Label();
+            this.labNombreEstado = new System.Windows.Forms.Label();
+            this.labMunicipio = new System.Windows.Forms.Label();
+            this.cmbMunicipio = new System.Windows.Forms.ComboBox();
+            this.dgvInformacion = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chbFecha = new System.Windows.Forms.CheckBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.labFecha1 = new System.Windows.Forms.Label();
+            this.labFecha2 = new System.Windows.Forms.Label();
+            this.datepick = new System.Windows.Forms.DateTimePicker();
+            this.ofdCargar = new System.Windows.Forms.OpenFileDialog();
+            this.sstAfiliados = new System.Windows.Forms.StatusStrip();
+            this.tsslAfiliados = new System.Windows.Forms.ToolStripStatusLabel();
+            this.msMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInformacion)).BeginInit();
+            this.sstAfiliados.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // msMenu
+            // 
+            this.msMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.opcionesToolStripMenuItem});
+            this.msMenu.Location = new System.Drawing.Point(0, 0);
+            this.msMenu.Name = "msMenu";
+            this.msMenu.Size = new System.Drawing.Size(801, 24);
+            this.msMenu.TabIndex = 0;
+            this.msMenu.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cargarToolStripMenuItem,
+            this.salirToolStripMenuItem1});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // cargarToolStripMenuItem
+            // 
+            this.cargarToolStripMenuItem.Name = "cargarToolStripMenuItem";
+            this.cargarToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.cargarToolStripMenuItem.Text = "Cargar Excel";
+            this.cargarToolStripMenuItem.Click += new System.EventHandler(this.cargarToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem1
+            // 
+            this.salirToolStripMenuItem1.Name = "salirToolStripMenuItem1";
+            this.salirToolStripMenuItem1.Size = new System.Drawing.Size(139, 22);
+            this.salirToolStripMenuItem1.Text = "Salir";
+            this.salirToolStripMenuItem1.Click += new System.EventHandler(this.salirToolStripMenuItem1_Click);
+            // 
+            // opcionesToolStripMenuItem
+            // 
+            this.opcionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.limpiarDatosToolStripMenuItem});
+            this.opcionesToolStripMenuItem.Name = "opcionesToolStripMenuItem";
+            this.opcionesToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.opcionesToolStripMenuItem.Text = "Opciones";
+            // 
+            // limpiarDatosToolStripMenuItem
+            // 
+            this.limpiarDatosToolStripMenuItem.Name = "limpiarDatosToolStripMenuItem";
+            this.limpiarDatosToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.limpiarDatosToolStripMenuItem.Text = "Limpiar datos";
+            this.limpiarDatosToolStripMenuItem.Click += new System.EventHandler(this.limpiarDatosToolStripMenuItem_Click);
+            // 
+            // labEstado
+            // 
+            this.labEstado.AutoSize = true;
+            this.labEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labEstado.Location = new System.Drawing.Point(14, 24);
+            this.labEstado.Name = "labEstado";
+            this.labEstado.Size = new System.Drawing.Size(79, 25);
+            this.labEstado.TabIndex = 1;
+            this.labEstado.Text = "Estado";
+            // 
+            // labNombreEstado
+            // 
+            this.labNombreEstado.AutoSize = true;
+            this.labNombreEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labNombreEstado.Location = new System.Drawing.Point(131, 28);
+            this.labNombreEstado.Name = "labNombreEstado";
+            this.labNombreEstado.Size = new System.Drawing.Size(240, 20);
+            this.labNombreEstado.TabIndex = 2;
+            this.labNombreEstado.Text = "No se ha cargado ningún archivo";
+            // 
+            // labMunicipio
+            // 
+            this.labMunicipio.AutoSize = true;
+            this.labMunicipio.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labMunicipio.Location = new System.Drawing.Point(14, 61);
+            this.labMunicipio.Name = "labMunicipio";
+            this.labMunicipio.Size = new System.Drawing.Size(104, 25);
+            this.labMunicipio.TabIndex = 3;
+            this.labMunicipio.Text = "Municipio";
+            // 
+            // cmbMunicipio
+            // 
+            this.cmbMunicipio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbMunicipio.FormattingEnabled = true;
+            this.cmbMunicipio.Items.AddRange(new object[] {
+            "Ninguno"});
+            this.cmbMunicipio.Location = new System.Drawing.Point(135, 65);
+            this.cmbMunicipio.Name = "cmbMunicipio";
+            this.cmbMunicipio.Size = new System.Drawing.Size(130, 24);
+            this.cmbMunicipio.Sorted = true;
+            this.cmbMunicipio.TabIndex = 4;
+            // 
+            // dgvInformacion
+            // 
+            this.dgvInformacion.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInformacion.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dgvInformacion.Location = new System.Drawing.Point(0, 95);
+            this.dgvInformacion.Name = "dgvInformacion";
+            this.dgvInformacion.Size = new System.Drawing.Size(794, 236);
+            this.dgvInformacion.TabIndex = 5;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "ID";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Entidad";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Municipio";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Nombre";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 250;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Fecha de Afiliación";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Estatus";
+            this.Column6.Name = "Column6";
+            // 
+            // chbFecha
+            // 
+            this.chbFecha.AutoSize = true;
+            this.chbFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chbFecha.Location = new System.Drawing.Point(17, 344);
+            this.chbFecha.Name = "chbFecha";
+            this.chbFecha.Size = new System.Drawing.Size(64, 20);
+            this.chbFecha.TabIndex = 6;
+            this.chbFecha.Text = "Fecha";
+            this.chbFecha.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Enabled = false;
+            this.dateTimePicker1.Location = new System.Drawing.Point(120, 367);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // labFecha1
+            // 
+            this.labFecha1.AutoSize = true;
+            this.labFecha1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labFecha1.Location = new System.Drawing.Point(16, 367);
+            this.labFecha1.Name = "labFecha1";
+            this.labFecha1.Size = new System.Drawing.Size(79, 16);
+            this.labFecha1.TabIndex = 8;
+            this.labFecha1.Text = "Fecha Inicio";
+            // 
+            // labFecha2
+            // 
+            this.labFecha2.AutoSize = true;
+            this.labFecha2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labFecha2.Location = new System.Drawing.Point(16, 397);
+            this.labFecha2.Name = "labFecha2";
+            this.labFecha2.Size = new System.Drawing.Size(98, 16);
+            this.labFecha2.TabIndex = 9;
+            this.labFecha2.Text = "Fecha Término";
+            // 
+            // datepick
+            // 
+            this.datepick.Enabled = false;
+            this.datepick.Location = new System.Drawing.Point(120, 397);
+            this.datepick.Name = "datepick";
+            this.datepick.Size = new System.Drawing.Size(200, 20);
+            this.datepick.TabIndex = 10;
+            // 
+            // ofdCargar
+            // 
+            this.ofdCargar.FileName = "Archivo | .xlsx";
+            this.ofdCargar.InitialDirectory = "Descargas";
+            // 
+            // sstAfiliados
+            // 
+            this.sstAfiliados.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsslAfiliados});
+            this.sstAfiliados.Location = new System.Drawing.Point(0, 444);
+            this.sstAfiliados.Name = "sstAfiliados";
+            this.sstAfiliados.Size = new System.Drawing.Size(801, 22);
+            this.sstAfiliados.TabIndex = 11;
+            // 
+            // tsslAfiliados
+            // 
+            this.tsslAfiliados.Name = "tsslAfiliados";
+            this.tsslAfiliados.Size = new System.Drawing.Size(59, 17);
+            this.tsslAfiliados.Text = "Afiliados :";
+            // 
+            // frmMilitantes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 466);
+            this.Controls.Add(this.sstAfiliados);
+            this.Controls.Add(this.datepick);
+            this.Controls.Add(this.labFecha2);
+            this.Controls.Add(this.labFecha1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.chbFecha);
+            this.Controls.Add(this.dgvInformacion);
+            this.Controls.Add(this.cmbMunicipio);
+            this.Controls.Add(this.labMunicipio);
+            this.Controls.Add(this.labNombreEstado);
+            this.Controls.Add(this.labEstado);
+            this.Controls.Add(this.msMenu);
+            this.MainMenuStrip = this.msMenu;
+            this.Name = "frmMilitantes";
+            this.Text = "Militantes Politicos";
+            this.msMenu.ResumeLayout(false);
+            this.msMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInformacion)).EndInit();
+            this.sstAfiliados.ResumeLayout(false);
+            this.sstAfiliados.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip msMenu;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem cargarToolStripMenuItem;
+        private System.Windows.Forms.Label labEstado;
+        private System.Windows.Forms.Label labNombreEstado;
+        private System.Windows.Forms.Label labMunicipio;
+        private System.Windows.Forms.ComboBox cmbMunicipio;
+        private System.Windows.Forms.ToolStripMenuItem opcionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem limpiarDatosToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dgvInformacion;
+        private System.Windows.Forms.CheckBox chbFecha;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label labFecha1;
+        private System.Windows.Forms.Label labFecha2;
+        private System.Windows.Forms.DateTimePicker datepick;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.OpenFileDialog ofdCargar;
+        private System.Windows.Forms.StatusStrip sstAfiliados;
+        private System.Windows.Forms.ToolStripStatusLabel tsslAfiliados;
+    }
+}
+
---
+++ Militantes politicos/Forms/frmMilitantes.cs
@@ -0,0 +1,103 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Militantes_politicos
+{
+    public partial class frmMilitantes : Form
+    {
+        List<string> columnas;
+        int afiliados = 1;
+        public frmMilitantes()
+        {
+            InitializeComponent();
+            columnas = new List<string> { "ID", "Entidad", "Municipio", "Nombre", "Fecha de afiliacion", "Estatus" };
+        }
+
+        private void CargarExcel(String path)
+        {
+            afiliados = 1;
+            DataTable dt = new DataTable();
+            ExcelPackage.License.SetNonCommercialPersonal("Josue Guzman");
+            using (var package = new ExcelPackage(new System.IO.FileInfo(path)))
+            {
+                if (package.Workbook.Worksheets.Count == 0)
+                {
+                    MessageBox.Show("El archivo de Excel no contiene hojas de trabajo.");
+                    return;
+                }
+
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+
+                foreach (var col in columnas)
+                {
+                    dt.Columns.Add(col);
+                }
+
+                int rowCount = 0;
+                for (int i = 2; i < worksheet.Dimension.End.Row; i++)
+                {
+                    rowCount = rowCount + 1;
+                    afiliados++;
+                }
+                rowCount = rowCount + 3;
+                for (int i = 2; i < rowCount; i++)
+                {
+                    DataRow row = dt.NewRow();
+                    for (int j = 1; j < dt.Columns.Count + 1; j++)
+                    {
+                        row[j - 1] = worksheet.Cells[i, j].Text;
+                    }
+                    dt.Rows.Add(row);
+                }
+
+
+                dgvInformacion.Invoke((MethodInvoker)delegate
+                {
+                    dgvInformacion.Rows.Clear();
+                    dgvInformacion.Rows.Add(dt.Rows.Count);
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        for (int j = 0; j < 6; j++)
+                        {
+                            dgvInformacion.Rows[i].Cells[j].Value = dt.Rows[i][j].ToString();
+                        }
+
+                    }
+                });
+            }
+        }
+
+        private void cargarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(ofdCargar.ShowDialog() == DialogResult.OK)
+            {
+                string archivo = ofdCargar.FileName;
+                Thread t1 = new Thread(() => CargarExcel(archivo));
+                t1.Start();
+                tsslAfiliados.Text = "Afiliados: " + afiliados.ToString();
+                labNombreEstado.Text = "Estado: " + dgvInformacion.Rows[0].Cells[1].Value.ToString();
+            }
+        }
+
+        private void limpiarDatosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dgvInformacion.Rows.Clear();
+            labNombreEstado.Text = "No se ha cargado ningún archivo";
+            tsslAfiliados.Text = "Afiliados: ";
+        }
+
+        private void salirToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
